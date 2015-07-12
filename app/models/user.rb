7559b5656e4f0c289a38286aa2a7896f5d2d9701
宮崎 善史 <yfm4423@gmail.com>
2015-07-12 11:16:31 +0000

    Finish user follow / unfollow actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,8 @@ class User < ActiveRecord::Base
   def follow!(other_user)
     relationships.create!(followed_id: other_user.id)
   end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
